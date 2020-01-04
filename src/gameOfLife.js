28dc9c8d340d8a78c89d9a2915f4eb6912eee3ce
aburd <burdick.aaron@gmail.com>
2020-01-04 21:53:50 +0900

    get controls and tick working

--- src/gameOfLife.js
+++ src/gameOfLife.js
@@ -1,9 +1,12 @@
 const WIDTH = 64;
 const HEIGHT = 64;
-const CELL_SIZE = 10;
-const CELL_STROKE = '#000';
+const CELL_SIZE = 8;
+const CELL_ALIVE_STROKE = '#77ccaa';
+const CELL_DEAD_STROKE = '#FFFFFF';
+const colors = [CELL_DEAD_STROKE, CELL_ALIVE_STROKE]
 const BORDER = 1;
-const BORDER_STROKE = '#666';
+const BORDER_STROKE = '#ddd';
+const MAX_FRAMERATE = 60;
 
 const Cell = {
     Alive: 1,
@@ -14,13 +17,14 @@ class Universe {
     constructor() {
         this.width = WIDTH;
         this.height = HEIGHT;
-        this.cells = this.createCells()
+        this.cells = this.createCells();
     }
 
-    createCells() {
-        return new Array(this.width * this.height).fill(null).map((_, i) => {
-            return i % 2 ? Cell.Alive : Cell.Dead;
-        })
+    createCells(seed = 2) {
+        let newCells = new Array(this.width * this.height)
+            .fill(null)
+            .map((_, i) => i % seed ? Cell.Alive : Cell.Dead);
+        return newCells
     }
 
     getCellIndex(row, col) {
@@ -36,12 +40,44 @@ class Universe {
         return str;
     }
 
+    tick() {
+        let newCells = [];
+        for(let row = 0; row < this.height; row++) {
+            for(let col = 0; col < this.width; col++) {
+                const idx = this.getCellIndex(row, col);
+                const neighbors = this.findCellNeighbors(row, col);
+                newCells[idx] = this.rules(this.cells[idx], neighbors);
+            }
+        }
+        this.cells = newCells;
+    }
+
+    rules(cell, neighbors) {
+        // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+        if(cell === Cell.Alive && neighbors < 2) {
+            return Cell.Dead;
+        }
+        // Any live cell with two or three live neighbours lives on to the next generation.
+        if(cell === Cell.Alive && (neighbors === 2) || (neighbors === 3)) {
+            return Cell.Alive;
+        }
+        // Any live cell with more than three live neighbours dies, as if by overpopulation.
+        if(cell === Cell.Alive && neighbors > 3) {
+            return Cell.Dead;
+        }
+        // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+        if(cell === Cell.Dead && neighbors === 3) {
+            return Cell.Alive;
+        }
+        return cell;
+    }
+
     findCellNeighbors(row, col) {
         let neighbors = 0;
-        for(let i = 0; i < 3; i++) {
-            const neighborRow = (row - 1 + i + this.height) % this.height;
-            for(let j = 0; j < 3; j++) {
-                const neighborCol = (col - 1 + j + this.width) % this.width;
+        for(let i = -1; i <= 1; i++) {
+            const neighborRow = (row + i + this.height) % this.height;
+            for(let j = -1; j <= 1; j++) {
+                const neighborCol = (col + j + this.width) % this.width;
                 if(neighborRow === row && neighborCol === col) continue;
                 const idx = this.getCellIndex(neighborRow, neighborCol)
                 neighbors += this.cells[idx];
@@ -52,31 +88,83 @@ class Universe {
 }
 
 function drawCells(universe, ctx) {
-    const { cells } = universe;
-    for(let i = 0; i < cells.length; i++) {
-        // ctx
-    }
-}
+    ctx.beginPath();
 
-function drawGrid(universe, ctx) {
-    ctx.strokeColor = BORDER_STROKE;
-    ctx.lineWidth = BORDER;
-    for(let row = 0; row <= universe.height; row++) {
-        ctx.moveTo(0, row * CELL_SIZE + BORDER)
-        ctx.lineTo(universe.width * CELL_SIZE + BORDER, row * CELL_SIZE + BORDER)
-        for(let col = 0; col <= universe.height; col++) {
-            ctx.moveTo(col * CELL_SIZE + BORDER, 0)
-            ctx.lineTo(col * CELL_SIZE + BORDER, universe.height * CELL_SIZE + BORDER)
+    for(let row = 0; row < universe.height; row++) {
+        for(let col = 0; col < universe.width; col++) {
+            const idx = universe.getCellIndex(row, col);
+            const cell = universe.cells[idx]
+            ctx.fillStyle = colors[cell];
+            ctx.fillRect(
+                col * (CELL_SIZE + BORDER) + BORDER,
+                row * (CELL_SIZE + BORDER) + BORDER,
+                CELL_SIZE,
+                CELL_SIZE,
+            )
         }
     }
+
     ctx.stroke()
 }
 
-export default function(canvas) {
+function drawGrid(universe, ctx) {
+    ctx.beginPath();
+
+    ctx.strokeStyle = BORDER_STROKE;
+    ctx.lineWidth = BORDER;
+
+    for(let row = 0; row <= universe.height; row++) {
+        ctx.moveTo(0, row * (CELL_SIZE + BORDER) + BORDER)
+        ctx.lineTo(universe.width * (CELL_SIZE + BORDER) + BORDER, row * (CELL_SIZE + BORDER) + BORDER)
+    }
+
+    for(let col = 0; col <= universe.width; col++) {
+        ctx.moveTo(col * (CELL_SIZE + BORDER) + BORDER, 0)
+        ctx.lineTo(col * (CELL_SIZE + BORDER) + BORDER, universe.height * (CELL_SIZE + BORDER) + BORDER)
+    }
+
+    ctx.stroke()
+}
+
+export default function({ 
+    canvas,
+    seedRange,
+    speedRange,
+ }) {
     const universe = new Universe();
-    canvas.width = universe.width * (CELL_SIZE + BORDER);
-    canvas.height = universe.height * (CELL_SIZE + BORDER);
+    canvas.width = universe.width * (CELL_SIZE + BORDER) + BORDER;
+    canvas.height = universe.height * (CELL_SIZE + BORDER) + BORDER;
     const ctx = canvas.getContext('2d');
+    let framerate = 15;
+
+    // Handlers
+    function changeSeed(ev) {
+        universe.cells = universe.createCells(ev.target.value);
+        drawCells(universe, ctx);
+    }
+    seedRange.addEventListener('input', changeSeed)
+    function changeSpeed(ev) {
+        framerate = (ev.target.value / 100) * MAX_FRAMERATE
+    }
+    speedRange.addEventListener('input', changeSpeed)
     
+    // Loop
     drawGrid(universe, ctx);
+    drawCells(universe, ctx);
+    let time = Date.now();
+    let timeBuffer = 0;
+    const loop = () => {
+        const now = Date.now();
+        const delta = now - time;
+        timeBuffer += delta;
+        time = now; 
+        if (timeBuffer > 1000 / framerate) {
+            universe.tick();
+            drawCells(universe, ctx);
+            timeBuffer = 0;
+        }
+
+        requestAnimationFrame(loop);
+    }
+    requestAnimationFrame(loop);
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
 import gameOfLife from './gameOfLife';
 
-const body = document.querySelector('body');
-const canvas = document.createElement('canvas');
+(function(){
+    const body = document.querySelector('body');
+    const canvas = document.createElement('canvas');
+    const seedRange = document.createElement('input');
+    seedRange.type = 'range';
+    const speedRange = document.createElement('input');
+    speedRange.type = 'range';
+    const controls = {
+        canvas,
+        seedRange,
+        speedRange,
+    };
 
-body.appendChild(canvas);
+    const mainContainer = document.createElement('div');
+    mainContainer.style = 'display: flex; flex-direction: column; width: 100%; text-align: center;'
 
-gameOfLife(canvas);
+    Object.entries(controls).forEach(([title, element]) => {
+        const container = document.createElement('div');
+        container.style = 'margin: 10px;'
+        const label = document.createElement('label');
+        label.style = 'display: block;'
+        label.textContent = title;
+
+        container.appendChild(label);
+        container.appendChild(element);
+        mainContainer.appendChild(container);
+    })
+
+    document.body.appendChild(mainContainer);
+
+    gameOfLife(controls);
+})()
